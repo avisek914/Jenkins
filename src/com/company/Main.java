92659706e7d744f8c1c9ff47122a91690771cc2f
avisek914 <avisek914@gmail.com>
2021-03-09 02:01:36 -0500

    error fixed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("This is just a simple program for Jenkin's class");
         System.out.println("Just for second commit");
-        ystem.out.println("3rd change");
+        System.out.println("3rd change");
     }
 }
